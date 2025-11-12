7ceca92ec48d5f644bb9b0f20e3bd61556802a46
gianmarcodalfonso <gianmarcoda10.gg@gmail.com>
2025-11-12 17:56:29 +0100

    feat: constructor

--- src/org/lessons/java/shop/Product.java
+++ src/org/lessons/java/shop/Product.java
@@ -1,4 +1,5 @@
 package org.lessons.java.shop;
+import java.util.Random;
 
 public class Product {
   //attributi
@@ -8,7 +9,18 @@ public class Product {
   float price;
   int iva;
 
-  
+  //costruttore
+  Product(String name, String description, float price) {
+    // genero codice random
+    java.util.Random r = new Random();
+    int codeMaxLength = 9999999;
+    this.code = r.nextInt(codeMaxLength);
+
+    // assegno i valori tramite il costruttore
+    this.name = name;
+    this.description = description;
+    this.price = price;
+  }
 
 
 }
